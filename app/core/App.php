8454dff8dad0c96d77780018f7f96a0fa8765303
efronpaduansi <eufrondpaduansi@gmail.com>
2022-08-15 14:58:53 +0700

    config app.php

--- app/core/App.php
+++ app/core/App.php
@@ -2,10 +2,32 @@
 
 class App{
 
+    // membuat controller, method dan params default
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
-        echo "Hello World";
+        
+        $url = $this->parseURL();
+        var_dump($url);
+        
     }
    
+    public function parseURL()
+    {
+        if( isset($_GET['url']))
+        {
+            // menghilangkan tanda slash (/) pada URL
+            $url = rtrim($_GET['url'], '/');
+            // menyaring karakter jahat pada URL
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // pecahkan URL-nya
+            $url = explode('/', $url);
+            return $url;
+
+        }
+    }
 
 }
